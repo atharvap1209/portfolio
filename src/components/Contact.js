3628b095a58753718966aa15c39a56536e5801ff
atharvap1209 <aapatkhedkar1209@gmail.com>
2023-09-05 23:19:45 +0530

    Second commit

---
+++ src/components/Contact.js
@@ -0,0 +1,129 @@
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme } from "./Themes";
+import { Design, Develope } from "./AllSvgs";
+
+import LogoComponent from "../subComponents/LogoComponent";
+import SocialIcons from "../subComponents/SocialIcons";
+import PowerButton from "../subComponents/PowerButton";
+import ParticleComponent from "../subComponents/ParticleComponent";
+import BigTitle from "../subComponents/BigTitlte";
+
+const Box = styled.div`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const Main = styled.div`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  background-color: ${(props) => props.theme.body};
+  padding: 2rem;
+  width: 30vw;
+  height: 60vh;
+  z-index: 3;
+  line-height: 1.5;
+  cursor: pointer;
+
+  font-family: "Ubuntu Mono", monospace;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  &:hover {
+    color: ${(props) => props.theme.body};
+    background-color: ${(props) => props.theme.text};
+  }
+`;
+
+const Title = styled.h2`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(1em + 1vw);
+
+  ${Main}:hover & {
+    & > * {
+      fill: ${(props) => props.theme.body};
+    }
+  }
+
+  & > *:first-child {
+    margin-right: 1rem;
+  }
+`;
+
+const Description = styled.div`
+  color: ${(props) => props.theme.text};
+  font-size: calc(0.6em + 1vw);
+  padding: 0.5rem 0;
+  text-align: center;
+  
+
+  ${Main}:hover & {
+    color: ${(props) => props.theme.body};
+  }
+
+  
+  ul,
+  p {
+    margin-left: 2rem;
+  }
+`;
+
+const Contact = () => {
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <Box>
+        <LogoComponent theme="light" />
+        <SocialIcons theme="light" />
+        <PowerButton />
+        <ParticleComponent theme="light" />
+
+        <Main>
+          <Title>You can reach out to me at</Title>
+          <Description>
+            <a
+              style={{ color: "inherit" }}
+              target="_blank"
+              href={"https://www.linkedin.com/in/atharva-ajit-patkhedkar/"}
+            >
+              LinkedIn
+            </a>
+          </Description>
+
+          <Description>
+            <a
+              style={{ color: "inherit" }}
+              target="_blank"
+              href={"mailto:aapatkhedkar1209@gmail.com"}
+            >
+              Email
+            </a>
+          </Description>
+
+          <Description>
+            <a
+              style={{ color: "inherit" }}
+              target="_blank"
+              href={
+                "https://drive.google.com/file/d/14kSgdPQg4p_5pchux5zPRwWzCCtEu3XH/view?usp=sharing"
+              }
+            >
+              My Resume
+            </a>
+          </Description>
+        </Main>
+
+        <BigTitle text="SKILLS" top="80%" right="30%" />
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+export default Contact;
---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+  {
+    id: 1,
+    name: "E-commerce website",
+    description:
+      "Build an E-commerce website using ReactJS and firebase, with login, and cart functionalities.",
+    tags: ["react", "firebase", "useContext API"],
+
+    demo: "https://my-ecommerce-vert-two.vercel.app/",
+    github: "https://github.com/atharvap1209/my-ecommerce-site",
+  },
+  {
+    id: 2,
+    name: "Job listing website",
+    description:
+      "A Job listing website build in ReactJS. With searching and filtering functionalities.",
+    tags: ["react", "sass"],
+
+    demo: "http://react-job-listing-website.codebucks27.vercel.app/",
+    github: "https://github.com/codebucks27/react-job-listing-website",
+  },
+  {
+    id: 3,
+    name: "Dev.to Clone",
+    description:
+      "A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
+    tags: ["react", "api", "sass"],
+
+    demo: "http://devto-clone-codebucks27.vercel.app/",
+    github: "https://github.com/codebucks27/devto-clone",
+  },
+  {
+    id: 4,
+    name: "Running a docker container with express.js",
+    description:"Created a docker container to sharpen my understanding of DevOps and Ran an Express.js basic setup, developed dockerfiles for staging and production environments.",
+    tags: ["express.js", "docker"],
+
+    demo: "",
+    github: "https://github.com/atharvap1209/docker-express",
+  },
+  {
+    id: 5,
+    name: "Todo App(React + Redux)",
+    description:
+      "A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+    tags: ["react", "redux", "framer-motion"],
+
+    demo: "http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+    github: "https://github.com/codebucks27/React-Redux-Todo-App",
+  },
+  {
+    id: 6,
+    name: "Responsive Navigation Menu",
+    description:
+      "A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+    tags: ["react", "react-router", "css"],
+
+    demo: "http://react-responsive-navbar-codebucks27.vercel.app/",
+    github: "https://github.com/codebucks27/React-responsive-navbar",
+  },
+]; 
